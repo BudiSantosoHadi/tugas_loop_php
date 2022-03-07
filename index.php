6f9380a032d1a2dc9f4d83cb98d987a2f7eaec99
atifnaufalh <atifnaufalh@gmail.com>
2022-03-07 13:51:08 +0700

    tugas looping php

---
+++ index.php
@@ -0,0 +1,718 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- css -->
+    <link rel="stylesheet" href="./css/stylesheet.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assingment</title>
+</head>
+<body>
+
+    <div class="big-container">
+    <table border= "1" cellpadding="8">
+          <thead class="hd_pg">
+          <th colspan="10">Logic 1.1</th>
+          </thead>
+         <tbody>
+         <tr>
+            <td class="none-box"></td>
+            <td class="num">1</td>
+            <td class="num">2</td>
+            <td class="num">3</td>
+            <td class="num">4</td>
+            <td class="num">5</td>
+            <td class="num">6</td>
+            <td class="num">7</td>
+            <td class="num">8</td>
+            <td class="num">9</td>
+          </tr>
+          <tr>
+            <td class="num">1</td>
+              <?php
+              echo logic_1();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">2</td>
+            <?php
+              echo logic_1();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">3</td>
+            <?php
+              echo logic_1();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">4</td>
+            <?php
+              echo logic_1();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">5</td>
+            <?php
+              echo logic_1();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">6</td>
+            <?php
+              echo logic_1();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">7</td>
+            <?php
+              echo logic_1();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">8</td>
+            <?php
+              echo logic_1();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">9</td>
+            <?php
+              echo logic_1();  
+              ?>
+          </tr>
+
+         </tbody>
+          
+        </table>
+        <table border= "1" cellpadding="8">
+          <thead class="hd_pg">
+          <th colspan="10">Logic 1.2</th>
+          </thead>
+         <tbody>
+         <tr>
+            <td class="none-box"></td>
+            <td class="num">1</td>
+            <td class="num">2</td>
+            <td class="num">3</td>
+            <td class="num">4</td>
+            <td class="num">5</td>
+            <td class="num">6</td>
+            <td class="num">7</td>
+            <td class="num">8</td>
+            <td class="num">9</td>
+          </tr>
+          <tr>
+            <td class="num">1</td>
+              <?php
+              echo logic_1();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">2</td>
+            <?php
+              echo logic_2();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">3</td>
+            <?php
+              echo logic_2();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">4</td>
+            <?php
+              echo logic_2();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">5</td>
+            <?php
+              echo logic_2();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">6</td>
+            <?php
+              echo logic_2();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">7</td>
+            <?php
+              echo logic_2();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">8</td>
+            <?php
+              echo logic_2();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">9</td>
+            <?php
+              echo logic_2();  
+              ?>
+          </tr>
+
+        </tbody>
+
+        </table>
+        <table border= "1" cellpadding="8">
+          <thead class="hd_pg">
+          <th colspan="10">Logic 1.3</th>
+          </thead>
+         <tbody>
+         <tr>
+            <td class="none-box"></td>
+            <td class="num">1</td>
+            <td class="num">2</td>
+            <td class="num">3</td>
+            <td class="num">4</td>
+            <td class="num">5</td>
+            <td class="num">6</td>
+            <td class="num">7</td>
+            <td class="num">8</td>
+            <td class="num">9</td>
+          </tr>
+          <tr>
+            <td class="num">1</td>
+              <?php
+              echo logic_3();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">2</td>
+            <?php
+              echo logic_3();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">3</td>
+            <?php
+              echo logic_3();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">4</td>
+            <?php
+              echo logic_3();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">5</td>
+            <?php
+              echo logic_3();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">6</td>
+            <?php
+              echo logic_3();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">7</td>
+            <?php
+              echo logic_3();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">8</td>
+            <?php
+              echo logic_3();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">9</td>
+            <?php
+              echo logic_3();  
+              ?>
+          </tr>
+
+         </tbody>
+          
+        </table>
+        <table border= "1" cellpadding="8">
+          <thead class="hd_pg">
+          <th colspan="10">Logic 1.4</th>
+          </thead>
+         <tbody>
+         <tr>
+            <td class="none-box"></td>
+            <td class="num">1</td>
+            <td class="num">2</td>
+            <td class="num">3</td>
+            <td class="num">4</td>
+            <td class="num">5</td>
+            <td class="num">6</td>
+            <td class="num">7</td>
+            <td class="num">8</td>
+            <td class="num">9</td>
+          </tr>
+          <tr>
+            <td class="num">1</td>
+              <?php
+              echo logic_1();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">2</td>
+            <?php
+              echo logic_4();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">3</td>
+            <?php
+              echo logic_4();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">4</td>
+            <?php
+              echo logic_4();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">5</td>
+            <?php
+              echo logic_4();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">6</td>
+            <?php
+              echo logic_4();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">7</td>
+            <?php
+              echo logic_4();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">8</td>
+            <?php
+              echo logic_4();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">9</td>
+            <?php
+              echo logic_4();  
+              ?>
+          </tr>
+
+         </tbody>
+          
+        </table>
+        <br>
+        <table border= "1" cellpadding="8">
+          <thead class="hd_pg">
+          <th colspan="10">Logic 1.5</th>
+          </thead>
+         <tbody>
+         <tr>
+            <td class="none-box"></td>
+            <td class="num">1</td>
+            <td class="num">2</td>
+            <td class="num">3</td>
+            <td class="num">4</td>
+            <td class="num">5</td>
+            <td class="num">6</td>
+            <td class="num">7</td>
+            <td class="num">8</td>
+            <td class="num">9</td>
+          </tr>
+          <tr>
+            <td class="num">1</td>
+              <?php
+              echo logic_1();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">2</td>
+            <?php
+              echo logic_5();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">2</td>
+            <?php
+              echo logic_5();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">4</td>
+            <?php
+              echo logic_5();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">5</td>
+            <?php
+              echo logic_5();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">6</td>
+            <?php
+              echo logic_5();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">7</td>
+            <?php
+              echo logic_5();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">8</td>
+            <?php
+              echo logic_5();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">9</td>
+            <?php
+              echo logic_5();  
+              ?>
+          </tr>
+
+         </tbody>
+          
+        </table>
+        <table border= "1" cellpadding="8">
+          <thead class="hd_pg">
+          <th colspan="10">Logic 1.6</th>
+          </thead>
+         <tbody>
+         <tr>
+            <td class="none-box"></td>
+            <td class="num">1</td>
+            <td class="num">2</td>
+            <td class="num">3</td>
+            <td class="num">4</td>
+            <td class="num">5</td>
+            <td class="num">6</td>
+            <td class="num">7</td>
+            <td class="num">8</td>
+            <td class="num">9</td>
+          </tr>
+          <tr>
+            <td class="num">1</td>
+              <?php
+              echo logic_1();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">2</td>
+            <?php
+              echo logic_6();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">2</td>
+            <?php
+              echo logic_6();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">4</td>
+            <?php
+              echo logic_6();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">5</td>
+            <?php
+              echo logic_1();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">6</td>
+            <?php
+              echo logic_6();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">7</td>
+            <?php
+              echo logic_6();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">8</td>
+            <?php
+              echo logic_6();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">9</td>
+            <?php
+              echo logic_1();  
+              ?>
+          </tr>
+
+         </tbody>
+          
+        </table>
+        <table border= "1" cellpadding="8">
+          <thead class="hd_pg">
+          <th colspan="10">Logic 1.7</th>
+          </thead>
+         <tbody>
+         <tr>
+            <td class="none-box"></td>
+            <td class="num">1</td>
+            <td class="num">2</td>
+            <td class="num">3</td>
+            <td class="num">4</td>
+            <td class="num">5</td>
+            <td class="num">6</td>
+            <td class="num">7</td>
+            <td class="num">8</td>
+            <td class="num">9</td>
+          </tr>
+          <tr>
+            <td class="num">1</td>
+              <?php
+              echo logic_1();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">2</td>
+            <?php
+              echo logic_6();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">2</td>
+            <?php
+              echo logic_6();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">4</td>
+            <?php
+              echo logic_6();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">5</td>
+            <?php
+              echo logic_1();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">6</td>
+            <?php
+              echo logic_6();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">7</td>
+            <?php
+              echo logic_6();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">8</td>
+            <?php
+              echo logic_6();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">9</td>
+            <?php
+              echo logic_1();  
+              ?>
+          </tr>
+
+         </tbody>
+          
+        </table>
+        <table border= "1" cellpadding="8">
+          <thead class="hd_pg">
+          <th colspan="10">Logic 1.8</th>
+          </thead>
+         <tbody>
+         <tr>
+            <td class="none-box"></td>
+            <td class="num">1</td>
+            <td class="num">2</td>
+            <td class="num">3</td>
+            <td class="num">4</td>
+            <td class="num">5</td>
+            <td class="num">6</td>
+            <td class="num">7</td>
+            <td class="num">8</td>
+            <td class="num">9</td>
+          </tr>
+          <tr>
+            <td class="num">1</td>
+              <?php
+              echo logic_1();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">2</td>
+            <?php
+              echo logic_6();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">2</td>
+            <?php
+              echo logic_6();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">4</td>
+            <?php
+              echo logic_6();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">5</td>
+            <?php
+              echo logic_1();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">6</td>
+            <?php
+              echo logic_6();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">7</td>
+            <?php
+              echo logic_6();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">8</td>
+            <?php
+              echo logic_6();  
+              ?>
+          </tr>
+          <tr>
+            <td class="num">9</td>
+            <?php
+              echo logic_1();  
+              ?>
+          </tr>
+
+         </tbody>
+          
+        </table>
+    </div>
+        
+    <?php
+    function logic_1(){
+      $result = "*";
+
+      for ($i=0; $i < 9; $i++) { 
+        echo"<td rowspan='1'>$result</td>";
+        
+      }
+    }
+
+    ?>
+
+    
+    <?php 
+    function logic_2(){
+      $result = "-";
+    
+      for ($i=0; $i < 9; $i++) { 
+        echo"<td rowspan='1'>$result</td>";
+
+      }
+
+    }
+    
+    ?>
+
+    <?php
+    function logic_3(){
+        $result_1 = "*";
+        $result_2 = "-";
+
+        for ($i=0; $i <= 0; $i++) { 
+          echo "<td rowspan='1'>$result_1</td>";
+        }
+         for ($i=1; $i < 9 ; $i++) { 
+          echo "<td rowspan='1'>$result_2</td>";
+        }
+      }
+    
+    ?>
+     <?php
+    function logic_4(){
+        $result_1 = "*";
+        $result_2 = "-";
+
+        for ($i=1; $i < 9 ; $i++) { 
+          echo "<td rowspan='1'>$result_2</td>";
+        }
+        for ($i=9; $i <= 9; $i++) { 
+          echo "<td rowspan='1'>$result_1</td>";
+        }
+        
+      }
+    
+    ?>
+      <?php
+    function logic_5(){
+        $result_1 = "*";
+        $result_2 = "-";
+
+        for ($i=9; $i <= 9; $i++) { 
+          echo "<td rowspan='1'>$result_1</td>";
+        }
+        for ($i=1; $i < 9 ; $i++) { 
+          echo "<td rowspan='1'>$result_2</td>";
+        }
+       
+        
+      }
+    
+    ?>
+       <?php
+    function logic_6(){
+        $result_1 = "*";
+        $result_2 = "-";
+
+
+        
+        for ($i=9; $i <= 9; $i++) { 
+          echo "<td rowspan='1'>$result_1</td>";
+        }
+        for ($i=5; $i < 8 ; $i++) { 
+          echo "<td rowspan='1'>$result_2</td>";
+        }
+        for ($i=5; $i <= 5; $i++) { 
+          echo "<td rowspan='1'>$result_1</td>";
+        }
+        for ($i=6; $i <= 8; $i++) { 
+          echo "<td rowspan='1'>$result_2</td>";
+        }
+        for ($i=9; $i <= 9; $i++) { 
+          echo "<td rowspan='1'>$result_1</td>";
+        }
+        
+       
+        
+      }
+    
+    ?>
+</body>
+</html>
